50651423315f5086134c3a6e2829f494f594b1e6
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-12-03 11:58:13 -0500

    Task GUI

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,35 @@
 
 public class Task {
     
+    protected String name, description;
+    
+    public Task(String nm, String d){
+        name = nm;
+        description = d;
+    }
+
+    public boolean validate() {
+        if (name == null || description == null || name.equals("") || description.equals("")) return false;
+        else return true;
+    }
+    
+    public String toString(){
+        return "Name: " + name + "\nDescription: " + description + "\n========\n";
+    }
+    
+    //getter & setter for name
+    public void setName(String nm) {
+            name = nm;
+    }
+    public final String getName() {
+        return name;
+    }
+    
+    //getter & setter for description
+    public void setDescription(String d) {
+            description = d;
+    }
+    public final String getDescription() {
+        return description;
+    }
 }
